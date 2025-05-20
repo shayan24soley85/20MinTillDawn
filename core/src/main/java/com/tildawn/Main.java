5a02ed760698c7d16e40b1bad4f922c2b158faf0
Your Name <you@example.com>
2025-05-20 12:44:29 +0330

    main changes in the main class!

--- core/src/main/java/com/tildawn/Main.java
+++ core/src/main/java/com/tildawn/Main.java
@@ -3,6 +3,7 @@ package com.tildawn;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,11 +11,13 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-    private SpriteBatch batch;
+    private static Main main;
+    private static SpriteBatch batch;
     private Texture image;
 
     @Override
     public void create() {
+        main=this;
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
     }
@@ -23,7 +26,7 @@ public class Main extends Game {
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 600, 210);
         batch.end();
     }
 
@@ -31,5 +34,29 @@ public class Main extends Game {
     public void dispose() {
         batch.dispose();
         image.dispose();
+    }
+
+    public static Main getMain() {
+        return main;
+    }
+
+    public static void setMain(Main main) {
+        Main.main = main;
+    }
+
+    public static SpriteBatch getBatch() {
+        return batch;
+    }
+
+    public static void setBatch(SpriteBatch batch) {
+        Main.batch = batch;
+    }
+
+    public Texture getImage() {
+        return image;
+    }
+
+    public void setImage(Texture image) {
+        this.image = image;
     }
 }
